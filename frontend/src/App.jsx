4cd018cadfe31923eff946d69d37636549cd518f
vishrutcogwave <vih5gamer@gmail.com>
2026-03-03 10:43:46 +0530

    policy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-
-
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HotelDetailsPage from './components/pages/HotelDetailsPage'
-import ApiTestPage from './components/pages/ApiTestPage'
-import CheckoutPage from './components/pages/CheckoutPage'
-import PaymentSuccessPage from './components/pages/PaymentSuccessPage'
-import HomeRedirect from './components/pages/HomeRedirect'
-import { ToastProvider } from './components/context/ToastContext'
-import ToastContainer from './components/ToastContainer'
-import PrivacyPolicyPage from './components/pages/PrivacyPolicyPage'
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HotelDetailsPage from "./components/pages/HotelDetailsPage";
+import ApiTestPage from "./components/pages/ApiTestPage";
+import CheckoutPage from "./components/pages/CheckoutPage";
+import PaymentSuccessPage from "./components/pages/PaymentSuccessPage";
+import HomeRedirect from "./components/pages/HomeRedirect";
+import { ToastProvider } from "./components/context/ToastContext";
+import ToastContainer from "./components/ToastContainer";
+import PrivacyPolicyPage from "./components/pages/PrivacyPolicyPage";
+import RefundPolicyPage from "./components/pages/RefundPolicyPage";
+import TermsAndConditionsPage from "./components/pages/TermsAndConditionsPage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -22,11 +23,16 @@ function App() {
           <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/payment-success" element={<PaymentSuccessPage />} />
           <Route path="/api-test" element={<ApiTestPage />} />
-         <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/refund-policy" element={<RefundPolicyPage />} />
+          <Route
+            path="/terms-conditions"
+            element={<TermsAndConditionsPage />}
+          />
         </Routes>
       </Router>
     </ToastProvider>
-  )
+  );
 }
 
-export default App
+export default App;
